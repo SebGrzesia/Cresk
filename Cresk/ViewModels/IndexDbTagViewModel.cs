784109bac883f0f3f332c4198885bdb080113052
SebastianG <dicoud@gmail.com>
2022-11-17 15:40:45 +0100

    Added indexDbTagViewModel

---
+++ Cresk/ViewModels/IndexDbTagViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cresk.ViewModels
+{
+    public class IndexDbTagViewModel
+    {
+        public string Id { get; set; }
+        [Display(Name = "Title")]
+        public string Name { get; set; }
+        [Display(Name= "Description")]
+        public string Desctiption { get; set; }
+    }
+}
