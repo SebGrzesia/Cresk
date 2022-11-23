7421ce95524fbd63e6f390c77f3c3e0608f2fbf9
SebastianG <dicoud@gmail.com>
2022-11-23 17:02:52 +0100

    Login page in progress

---
+++ Cresk/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace Cresk.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Cresk/ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cresk.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
