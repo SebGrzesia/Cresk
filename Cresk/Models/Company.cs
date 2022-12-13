eb8e20249ebcf24b1dbd3fbff6a2b8c612311a5a
Sebastian <dicoud@gmail.com>
2022-12-13 13:02:09 +0100

    Company in progress

---
+++ Cresk/Models/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace Cresk.Models
+{
+    public class Company
+    {
+        public string Id { get; set; }
+        public string CompanyName { get; set; }
+        public Company()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
